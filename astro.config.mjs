bc5ac5386eadfe4d57ce8e99c70e856ad43de4e8
Chris Budzban <chris@elexicon.com>
2024-07-21 14:30:08 -0500

    re-add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://budzo.github.io',
+  base: '/astro-test',
   integrations: [tailwind()]
 });
